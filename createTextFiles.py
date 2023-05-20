47d66c011a2674c25717a1cc71826697e6ffb877
Om <om.ambarkar@gmail.com>
2023-05-20 19:42:49 +0530

    first Commit

--- createTextFiles.py
+++ createTextFiles.py
@@ -3,9 +3,10 @@ import os
 
 def createTxtFiles(file_path: str, file_name: str):
     file_name_initial = file_name.split(".")[0]
-    final_directory = os.path.join(file_path, file_name_initial+".txt")
-    file = open(final_directory, 'w')
-    file.write('Created on:')
-    file.close()
-    print(f"Created file named:{final_directory}")
-    # deleteFromDirectory(os.path.join(file_path, file_name))
+    file_name_end = file_name.split(".")[1]
+    if (file_name_end != "txt"):
+        final_directory = os.path.join(file_path, file_name_initial+".txt")
+        file = open(final_directory, 'w')
+        file.write('Created on:')
+        file.close()
+        print(f"Created file named:{final_directory}")
